3ac909bb85421770bae12e53b66a7eaf2fae2cc6
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-01-28 11:30:59 +0700

    Update PlaceForm.vue

--- src/views/components/KlineTrade/PlaceForm.vue
+++ src/views/components/KlineTrade/PlaceForm.vue
@@ -416,7 +416,7 @@ export default {
         this.address,
         txMsgs,
         txFee,
-        'Sent Via https://ping.pub',
+        'Sent Via https://vinjanxplorer.jepe.biz.id',
         signerData,
       ).then(bodyBytes => {
         this.$http.broadcastTx(bodyBytes).then(res => {
