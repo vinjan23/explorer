8e199a8f7bfb2d8e3f321221cd46002eaee387d9
vinjan <108977419+vinjan23@users.noreply.github.com>
2023-01-28 12:03:02 +0700

    Update index.js

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -19,7 +19,7 @@ function processMenu() {
       title: 'blockchains',
       children: blockchains,
       tag: `${blockchains.length}`,
-      icon: 'https://ping.pub/logo.svg',
+      icon: 'https://https://vinjanxplorer.jepe.biz.id//logox.svg',
     })
   }
   chainMenus.push({ header: 'LINKS' })
