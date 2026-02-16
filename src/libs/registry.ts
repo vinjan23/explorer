179dc3223408851c6cd915cb6656a897560a3a7b
vinjan <am.setiawan23@gmail.com>
2026-02-16 12:55:30 +0700

    Update registry.ts

--- src/libs/registry.ts
+++ src/libs/registry.ts
@@ -1,4 +1,7 @@
+import { Registry } from "@cosmjs/proto-signing";
 import { EthSecp256k1PubKey } from "@evmos/proto/ethermint/crypto/v1/ethsecp256k1/keys";
+
+export const registry = new Registry();
 
 registry.register(
   "/ethermint.crypto.v1.ethsecp256k1.Pubkey",
