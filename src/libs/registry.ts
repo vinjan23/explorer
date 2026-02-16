910c7c044a87cef1e0865b78bfc678dadc95248b
vinjan <am.setiawan23@gmail.com>
2026-02-16 12:50:54 +0700

    Create registry.ts

---
+++ src/libs/registry.ts
@@ -0,0 +1,6 @@
+import { EthSecp256k1PubKey } from "@evmos/proto/ethermint/crypto/v1/ethsecp256k1/keys";
+
+registry.register(
+  "/ethermint.crypto.v1.ethsecp256k1.Pubkey",
+  EthSecp256k1PubKey
+);
